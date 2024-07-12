03988407ace4fd8d96b8b9353d0b1ada26935261
Sten Schmidt <sten.schmidt@uscca.com>
2024-07-12 12:45:32 -0500

    g

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get('/', function (req, res) {
 
 app.post('/api/fileanalyse', upload.single('upfile'), function (req, res, next) {
   console.log('req.body vvvvv');
-  console.dir(req);
+  console.log(req);
   let num = 1;
   res.json({
     name: "upfi",
