ac432eff300b4cbd6b971e02f1fe772efbba9bd5
Sten Schmidt <sten.schmidt@uscca.com>
2024-07-15 09:04:14 -0500

    g

--- index.js
+++ index.js
@@ -20,10 +20,10 @@ app.post('/api/fileanalyse', upload.single('upfile'), function (req, res, next) 
   console.log('req.file.fieldname: ' + req.file.fieldname);
   console.log('req.file.mimetype: ' + req.file.mimetype);
   console.log('req.file.size: ' + req.file.size);
-  res.send({
-    "name": req.file.fieldname,
-    "type": req.file.mimetype,
-    "size": Number(req.file.size)
+  res.json({
+    name: req.file.fieldname,
+    type: req.file.mimetype,
+    size: Number(req.file.size)
   });
 });
 
